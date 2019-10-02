311bcc58943826e104ecfd6f5ebe2378e6cf4069
zelalemzergaw <zola145103@gmail.com>
2019-10-01 23:00:21 -0500

    comit 2

--- java codding Assignment/src/edu/mum/cs/cs425/ex0/crmapp/model/Customer.java
+++ java codding Assignment/src/edu/mum/cs/cs425/ex0/crmapp/model/Customer.java
@@ -1,4 +1,53 @@
 package edu.mum.cs.cs425.ex0.crmapp.model;
 
+import java.time.LocalDate;
+
 public class Customer {
+    private int id;
+
+    private String name;
+
+    private LocalDate dateOfBirth;
+
+    public Customer() {
+    }
+
+    public Customer(int id, String name, LocalDate dateOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
 }
