11ca138ee8e623b66e500248171a2be8d8dae87e
zelalemzergaw <zola145103@gmail.com>
2019-10-10 14:57:38 -0500

    first commit 2

---
+++ Lab 7 ORM JPA/src/main/java/edu/mum/cs/cs425/studentmgmt/studentmgmt/StudentmgmtApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.cs425.studentmgmt.studentmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentmgmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentmgmtApplication.class, args);
+    }
+
+}
