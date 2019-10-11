f42266b385f8255bab399086dccb1a71f99f4c9d
zelalemzergaw <zola145103@gmail.com>
2019-10-11 02:54:14 -0500

    commit 1

---
+++ exercisefromyoutube/src/main/java/com/edu/springexercise/exercisefromyoutube/exercisefromyoutube/ExercisefromyoutubeApplication.java
@@ -0,0 +1,13 @@
+package com.edu.springexercise.exercisefromyoutube.exercisefromyoutube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercisefromyoutubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExercisefromyoutubeApplication.class, args);
+    }
+
+}
