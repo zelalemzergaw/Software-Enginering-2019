9dd9bc74cbf054830dea7f9e0083ec64641661cc
zelalemzergaw <zola145103@gmail.com>
2019-10-11 20:05:38 -0500

    firstCommit 1

---
+++ studentdata/src/main/java/edu/mum/cs425/studentdata/StudentdataApplication.java
@@ -0,0 +1,18 @@
+package edu.mum.cs425.studentdata;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentdataApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentdataApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
