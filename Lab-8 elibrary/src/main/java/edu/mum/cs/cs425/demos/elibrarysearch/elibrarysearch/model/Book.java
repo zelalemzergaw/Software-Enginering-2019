947b418393541aba169405918b249574965fcf7e
zelalemzergaw <zola145103@gmail.com>
2019-10-13 02:06:26 -0500

    1 com 2

--- Lab-8 elibrary/src/main/java/edu/mum/cs/cs425/demos/elibrarysearch/elibrarysearch/model/Book.java
+++ Lab-8 elibrary/src/main/java/edu/mum/cs/cs425/demos/elibrarysearch/elibrarysearch/model/Book.java
@@ -1,17 +1,24 @@
 package edu.mum.cs.cs425.demos.elibrarysearch.elibrarysearch.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "book")
+
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer bokId;
 
+    @NotBlank(message = "*ISBN number is required")
+    @Column(name = "isbn")
     private String isbn;
 
+    @NotBlank(message = "*Book tile is required")
+    @Column(name = "title")
     private String title;
 
     private Double overDueFee;
@@ -20,4 +27,67 @@ public class Book {
 
     private LocalDate datePublished;
 
+
+    public Book() {
+    }
+
+    public Integer getBokId() {
+        return bokId;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Double getOverDueFee() {
+        return overDueFee;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public LocalDate getDatePublished() {
+        return datePublished;
+    }
+
+    public void setBokId(Integer bokId) {
+        this.bokId = bokId;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setOverDueFee(Double overDueFee) {
+        this.overDueFee = overDueFee;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public void setDatePublished(LocalDate datePublished) {
+        this.datePublished = datePublished;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bokId=" + bokId +
+                ", isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", overDueFee=" + overDueFee +
+                ", publisher='" + publisher + '\'' +
+                ", datePublished=" + datePublished +
+                '}';
+    }
 }
