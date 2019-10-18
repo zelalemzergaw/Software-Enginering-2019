579c9d026653a5c19ada7077eb5cc34c71987caa
Zelalem Zergaw <zola145103@gmail.com>
2019-10-18 01:32:05 -0500

    project started

---
+++ Banking/src/test/java/mum/edu/cs/cs425/banking/banking/BankingApplicationTests.java
@@ -0,0 +1,13 @@
+package mum.edu.cs.cs425.banking.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
